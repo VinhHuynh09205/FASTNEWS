4b3b7d396fcbd8f4add16f3baec1df7e62863e76
nhathuymario <huyne1501@gmail.com>
2025-03-30 13:27:13 +0700

    hello web

--- news/includes/master.php
+++ news/includes/master.php
@@ -7,6 +7,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?? ""; ?></title>
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/index.css">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+    />
+    <script src="<?php echo BASE_URL; ?>assets/js/main.js"></script>
 </head>
 <body>
 
