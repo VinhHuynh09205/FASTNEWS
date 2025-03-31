bb2a4bac8545fe08e6d46a05c2bd36e83264557a
Vinh huỳnh <huynhngocquangvinhh@gmail.com>
2025-03-31 16:39:16 +0700

    UPDATE CONTENT

--- news/index.php
+++ news/index.php
@@ -4,11 +4,11 @@ $title = "Trang Chủ";
 $content =
     <<<HTML
     <main>
-    <h2>BÀI VIẾT NỔI BẬT</h2><hr>
+    <h2>BÀI VIẾT NỔI BẬT</h2>
     <div class="news-section">
         <section class = "featured">
             <article>
-                <img src="assets/img/h1.webp" alt="Hình ảnh nổi bật">
+                <img src="assets/img/featured-news/h1.webp" alt="Hình ảnh nổi bật">
                 <h3><a href="#">WHO kích hoạt cơ chế khẩn để hỗ trợ Myanmar sau động đất 7,7 độ</a></h3>
                 <p>Chính quyền Myanmar ban bố tình trạng khẩn cấp tại 6 vùng sau trận động đất mạnh 7,7 độ xảy ra ở miền trung nước này, khiến nhiều công trình đổ sập và gây thương vong.</p>
                 <a href="#" class="a-doctiep">Đọc tiếp</a>
@@ -18,7 +18,7 @@ $content =
     <div class="news-section">
         <section class = "featured2">
             <article>
-                <img src="assets/img/h2.webp" alt="Hình ảnh nổi bật">
+                <img src="assets/img/featured-news/h2.webp" alt="Hình ảnh nổi bật">
                 <h3><a href="#">Hàng chục người về Đền Hùng trước ngày giỗ Tổ</a></h3>
                 <p>Phú ThọKhu di tích lịch sử Đền Hùng đón hàng chục nghìn lượt khách về hành lễ trong hai ngày 29 và 30/3, trước giỗ Tổ Hùng Vương hơn một tuần.</p>
                 <a href="#" class="a-doctiep">Đọc tiếp</a>
@@ -26,7 +26,7 @@ $content =
         </section>
         <section class = "featured3">
             <article>
-                <img src="assets/img/h3.webp" alt="Hình ảnh nổi bật">
+                <img src="assets/img/featured-news/h3.webp" alt="Hình ảnh nổi bật">
                 <h3><a href="#">Việt Nam viện trợ 300.000 USD giúp Myanmar khắc phục hậu quả động đất</a></h3>
                 <p>Việt Nam cung cấp đồ cứu trợ và viện trợ 300.000 USD nhằm giúp Myanmar khắc phục hậu quả sau trận động đất 7,7 độ.</p>
                 <a href="#" class="a-doctiep">Đọc tiếp</a>
@@ -38,48 +38,110 @@ $content =
         <section class="latest-news">
             <h2>TIN MỚI NHẤT</h2>
             <article>
-                <img src="assets/img/h4.webp" alt="Hình ảnh tin mới">
+                <img src="assets/img/latest-news/h1.webp" alt="Hình ảnh tin mới">
                 <h3><a href="#">Đất đấu giá huyện ven Hà Nội lại trúng hơn 100 triệu đồng một m2</a></h3>
                 <p>Hà NộiHuyện Quốc Oai vừa đấu giá thành công 26 lô đất tại xã Hưng Đạo, trong đó giá trúng cao nhất 104 triệu một m2 - tăng mạnh so với phiên trước.</p>
             </article>
             <hr>
             <article>
-                <img src="assets/img/h5.webp" alt="Hình ảnh tin mới">
+                <img src="assets/img/latest-news/h2.webp" alt="Hình ảnh tin mới">
                 <h3><a href="#">'Bộ đội Việt Nam sẽ tìm người mất tích ở Myanmar như tìm người thân'</a></h3>
                 <p>Thiếu tướng Phạm Văn Tỵ khẳng định bộ đội Việt Nam sẽ tìm kiếm nạn nhân bị vùi lấp sau thảm họa động đất tại Myanmar như tìm kiếm chính người thân của mình.</p>
             </article>
             <hr>
             <article>
-                <img src="assets/img/h6.webp" alt="Hình ảnh tin mới">
+                <img src="assets/img/latest-news/h3.webp" alt="Hình ảnh tin mới">
                 <h3><a href="#">Ôtô chở 36 khách rơi xuống vực đèo Bảo Lộc</a></h3>
                 <p>Lâm ĐồngXe chở khách từ Bình Phước lên Đà Lạt du lịch trên đường về gặp tai nạn, rơi xuống vực đèo Bảo Lộc, một nạn nhân tử vong, hơn 30 người bị thương, chiều 30/3.</p>
             </article>
+            <hr>
             <article>
-                <img src="assets/img/h7.webp" alt="Hình ảnh tin mới">
+                <img src="assets/img/latest-news/h4.webp" alt="Hình ảnh tin mới">
                 <h3><a href="#">Thủ tướng yêu cầu gỡ vướng dứt điểm cho hơn 1.500 dự án</a></h3>
                 <p>Thủ tướng yêu cầu gỡ vướng cho hơn 1.500 dự án tồn đọng kéo dài theo hướng "sai tới đâu xử lý đó", không để tạo tiền lệ xấu.</p>
             </article>
+            <hr>
             <article>
-                <img src="assets/img/h8.webp" alt="Hình ảnh tin mới">
+                <img src="assets/img/latest-news/h5.webp" alt="Hình ảnh tin mới">
                 <h3><a href="#">Myanmar đối mặt khủng hoảng nhân đạo sau động đất</a></h3>
                 <p>Hội Chữ thập Đỏ nói rằng Myanmar đang đối mặt khủng hoảng nhân đạo, nhu cầu viện trợ tăng từng giờ sau trận động đất làm khoảng 1.700 người chết.</p>
             </article>
+            <hr>
+            <article>
+                <img src="assets/img/latest-news/h6.webp" alt="Hình ảnh tin mới">
+                <h3><a href="#">Vì sao hơn 5,2 triệu hộ kinh doanh 'mãi không chịu lớn'?</a></h3>
+                <p>Khu vực hộ kinh doanh tạo ra 8-9 triệu việc làm, tương đương khối công ty tư nhân, nhưng họ "ngại lớn" bởi rào cản thuế, phí và năng lực.</p>
+            </article>
+            <hr>
+            <article>
+                <img src="assets/img/latest-news/h7.webp" alt="Hình ảnh tin mới">
+                <h3><a href="#">Tiến sĩ người Việt được bầu vào Viện Hàn lâm Khoa học trẻ toàn cầu</a></h3>
+                <p>Tiến sĩ Hoàng Anh Đức, 35 tuổi, được bầu vào Viện Hàn lâm Khoa học trẻ toàn cầu nhiệm kỳ 2025-2030 vì có nhiều thành tích trong nghiên cứu và chuyển đổi số giáo dục.</p>
+            </article>
+            <hr>
+            <article>
+                <img src="assets/img/latest-news/h8.webp" alt="Hình ảnh tin mới">
+                <h3><a href="#">Kim Soo Hyun: Tôi sống trong sợ hãi</a></h3>
+                <p>Hàn Quốc-Tài tử Kim Soo Hyun xuất hiện giữa ồn ào đời tư, khóc cho biết sống trong sợ hãi vì bị tung tin sai sự thật.</p>
+            </article>
+
         </section>
         <!-- Cột 2: Tin tức theo danh mục -->
         <section class="category-news">
             <h2>TIN THEO DANH MỤC</h2>
+            <h3><u>Thế giới</u></h3>
             <article>
-                <h3><a href="#">Tiêu đề bài viết 1</a></h3>
-                <p>Mô tả ngắn...</p>
+                <img src="assets/img/category-news/Thế giới/h1.webp" alt="Hình ảnh thế giới">
+                <h4><a href="#">Nhà Trắng sẽ chặt bỏ cây cổ thụ gần 200 tuổi</a></h4>
+                <p>Ông Trump thông báo cây mộc lan có tuổi đời gần 200 năm gần cổng vòm phía nam Nhà Trắng sẽ bị chặt bỏ vì đang mục ruỗng.</p>
             </article>
+            <hr>
             <article>
-                <h3><a href="#">Tiêu đề bài viết 2</a></h3>
-                <p>Mô tả ngắn...</p>
+                <img src="assets/img/category-news/Thế giới/h2.webp" alt="Hình ảnh thế giới">
+                <h4><a href="#">Cậu bé mắc ung thư tuyên thệ tại hơn 900 cơ quan hành pháp Mỹ</a></h4>
+                <p>DJ Daniel, cậu bé mắc ung thư não, đã được 914 cơ quan hành pháp Mỹ công nhận là thành viên danh dự, và đang đặt mục tiêu nâng con số này lên 1.000.</p>
             </article>
+            <hr>
+            <h3><u>Thể thao</u></h3>
             <article>
-                <h3><a href="#">Tiêu đề bài viết 3</a></h3>
-                <p>Mô tả ngắn...</p>
+                <img src="assets/img/category-news/Thể thao/h1.webp" alt="Hình ảnh thể thao">
+                <h4><a href="#">Jakub Mensik: 'Tôi chơi quần vợt vì Djokovic'</a></h4>
+                <p>Mỹ-Nhà vô địch Miami Mở rộng 2025 nói Novak Djokovic là cảm hứng để anh trở thành tay vợt chuyên nghiệp và phấn đấu cho những danh hiệu ATP.</p>
             </article>
+            <hr>
+            <article>
+                <img src="assets/img/category-news/Thể thao/h2.webp" alt="Hình ảnh thể thao">
+                <h4><a href="#">Roy Keane chê Haaland hời hợt</a></h4>
+                <p>Anh-Cựu tiền vệ Roy Keane bức xúc trước hình ảnh Erling Haaland mỉm cười sau khi hỏng ăn trong trận Man City thắng ngược Bournemouth 2-1 ở tứ kết Cup FA.</p>
+            </article>
+            <hr>
+            <h3><u>Công nghệ</u></h3>
+            <article>
+                <img src="assets/img/category-news/Công nghệ/h1.webp" alt="Hình ảnh Công nghệ">
+                <h4><a href="#">iPhone gập có thể dùng bản lề kim loại lỏng bền gấp đôi titan</a></h4>
+                <p>iPhone gập được cho là sử dụng kim loại lỏng làm bản lề, giúp bộ phận này có độ cứng vượt trội titan và vẻ ngoài bóng bẩy như thép.</p>
+            </article>
+            <hr>
+            <article>
+                <img src="assets/img/category-news/Công nghệ/h2.webp" alt="Hình ảnh Công nghệ">
+                <h4><a href="#">Thiết bị dự đoán cơn động kinh tích hợp AI</a></h4>
+                <p>Thiết bị đeo y tế có kích thước siêu nhỏ gọn, tích hợp trí tuệ nhân tạo AI để phân tích sóng não theo thời gian nhằm dự báo cơn động kinh.</p>
+            </article>
+            <hr>
+            <h3><u>Giải trí</u></h3>
+            <article>
+                <img src="assets/img/category-news/Giải trí/h1.webp" alt="Hình ảnh Giải trí">
+                <h4><a href="#">Loạt sao quốc tế diện đầm họa tiết của Vũ Ngọc và Son</a></h4>
+                <p>Diễn viên Sasheer Zamata, Sherry Cola, Skye Aurelia chọn trang phục voan, gấm họa tiết hoa lá của nhà thiết kế Việt lên thảm đỏ.</p>
+            </article>
+            <hr>
+            <article>
+                <img src="assets/img/category-news/Giải trí/h2.webp" alt="Hình ảnh Giải trí">
+                <h4><a href="#">Quang Tuấn giảm 14 kg để đóng 'Địa đạo'</a></h4>
+                <p>Quang Tuấn trải qua hai tháng rèn thể lực, giảm từ 77 kg còn 63 kg để vào vai người lính chiến đấu ở Củ Chi trong phim "Địa đạo".</p>
+            </article>
+
+
         </section>
     </div>
     <div class="news-section3">
